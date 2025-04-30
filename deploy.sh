fa2813e97480db86cee7c3af7d080dfbbbcc5e01
Tom Koreny <tom@tomkoreny.com>
2025-04-30 14:10:24 +0200

    feat: add my aurora corne config

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+qmk compile
+sleep 3
+sudo mkdir /tmp/kb
+sudo mount /dev/sde1 /tmp/kb
+sudo cp splitkb_aurora_corne_rev1_miryoku_553.uf2 /tmp/kb/fw.uf2
---
+++ users/miryoku_553/rules.mk
@@ -0,0 +1,15 @@
+# Copyright 2019 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
+
+include $(PWD)/users/miryoku_553/custom_rules.mk
+
+include $(PWD)/users/miryoku_553/post_rules.mk
